6991558c07cc73fe4819794e190ac9e05e6a42ab
Mate Forgac <465356@mail.muni.cz>
2021-07-01 10:23:05 +0200

    done

--- 1487C.cpp
+++ 1487C.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 
 tuple<int, int> find_team(vector<deque<int>> &adj, int desired_team){
-    for(vector<int>::size_type i = 1; i != adj.size(); i++){
+    for(vector<int>::size_type i = 1; i != adj.size(); ++i){
         if(adj[i][0] == 0){ // stop resizing
             adj[i].resize(0);
         }
-        for(auto j =0; j< adj[i].size();j++){
+        for(auto j =0; j< adj[i].size();++j){
             if(desired_team == adj[i][j]){
                 adj[i].pop_back();
                 return make_tuple(i, adj[i][j]);
@@ -26,7 +26,7 @@ tuple<int, int> find_team(vector<deque<int>> &adj, int desired_team){
 tuple<int, int> draws(vector<deque<int>> &adj){
 
     for(vector<int>::size_type i = 1; i != adj.size(); i++){
-        for(auto j =0; j < adj[i].size();j++) {
+        for(auto j =0; j < adj[i].size();++j) {
             adj[i].pop_back();
             return make_tuple(i, adj[i][j]);
             }
@@ -37,8 +37,8 @@ tuple<int, int> draws(vector<deque<int>> &adj){
 void solve(vector<deque<int>> adj, int num_teams){
     int num_iteration = (num_teams + 1) / 2;
     int scores[num_teams+1][num_teams+1];
-    for(int j = 0; j < num_teams +1; j++){
-        for(int k = 0; k < num_teams + 1;k++){
+    for(int j = 0; j < num_teams +1; ++j){
+        for(int k = 0; k < num_teams + 1;++k){
             scores[j][k] = -5;
         }
     }
@@ -55,7 +55,7 @@ void solve(vector<deque<int>> adj, int num_teams){
             second_team = first_team + i;
 
             if (i == num_iteration && num_teams % 2 == 0) { // last round if even, must be n/2 draws
-                for(auto k =0; k< num_teams /2;k++){
+                for(auto k =0; k< num_teams /2;++k){
                 tuple<int, int> draw_teams = draws(adj);
                 if (get<0>(draw_teams) != -1) {
                     scores[get<0>(draw_teams)][get<1>(draw_teams)] = 0;
@@ -77,8 +77,8 @@ void solve(vector<deque<int>> adj, int num_teams){
 
     }
 
-    for(int j = 1; j < num_teams +1; j++){
-        for(int k = 0; k < num_teams + 1;k++){
+    for(int j = 1; j < num_teams +1; ++j){
+        for(int k = 0; k < num_teams + 1;++k){
             if(scores[j][k] != -5){
                 cout << scores[j][k] << " ";
             }
@@ -90,8 +90,8 @@ void input(){
     int num_teams ;
     scanf("%d", &num_teams);
     vector<deque<int>> adj(num_teams+1);
-    for(int i = 1; i <= num_teams; i++){
-        for(int j = i + 1; j <= num_teams;j++){
+    for(int i = 1; i <= num_teams; ++i){
+        for(int j = i + 1; j <= num_teams;++j){
             adj[i].push_back(j);
         }
     }
@@ -103,7 +103,7 @@ void input(){
 int main(){
     int num_test;
     scanf("%d", &num_test);
-    for(int i =0; i < num_test;i++){
+    for(int i =0; i < num_test;++i){
         input();
     }
 }
